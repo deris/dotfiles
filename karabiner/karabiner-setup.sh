bd662223ec32b7634f0ec80375ce37594f8aff86
deris0126 <deris0126@gmail.com>
2015-05-01 12:10:58 +0900

    add setup karabiner

---
+++ karabiner/karabiner-setup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set repeat.initial_wait 50
+/bin/echo -n .
+$cli set remap.cmdtab_mode_controlM 1
+/bin/echo -n .
+$cli set remap.hhkmode_opt1_optEqual_to_f1_f12 1
+/bin/echo -n .
+$cli set remap.jis_escape2eisuuAndEscape 1
+/bin/echo -n .
+$cli set option.simultaneouskeypresses_vimode_f_to_fn 1
+/bin/echo -n .
+$cli set remap.swap_capslock_number_and_symbol 1
+/bin/echo -n .
+$cli set remap.jis_commandL2commandL_eisuu 1
+/bin/echo -n .
+$cli set remap.jis_command2eisuukana_prefer_command 1
+/bin/echo -n .
+$cli set remap.jis_yen2backslash 1
+/bin/echo -n .
+$cli set remap.holding_s_to_spaces 1
+/bin/echo -n .
+$cli set repeat.wait 10
+/bin/echo -n .
+$cli set remap.simultaneouskeypresses_cmdtab_tabq 1
+/bin/echo -n .
+$cli set remap.shiftr_to_sticky_shiftr 1
+/bin/echo -n .
+/bin/echo
